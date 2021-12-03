303468c11bc65ed593efed477c1676d38d417c7e
Roger Bücker <roger.buecker@arvato-scs.com>
2021-12-03 20:42:40 +0100

    first commit

---
+++ src/fleet/fleet.mock.ts
@@ -0,0 +1,6 @@
+import { Fleet } from '@/fleet'
+import { Team } from '@/team'
+import { mockGridFactory } from '@/grid'
+
+export const mockFleetFactory = (team = Team.A, grid = mockGridFactory()): Fleet =>
+  new Fleet(team, grid)
